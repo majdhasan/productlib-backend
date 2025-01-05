80181d6251018fbed84719bd20cf2e79705d7cec
Majd Hasan <majd.hasan@bettermile.com>
2025-01-05 06:24:43 +0100

    add auth service, user context holder and restrict orders

--- src/main/kotlin/com/meshhdawi/productlib/orders/OrdersController.kt
+++ src/main/kotlin/com/meshhdawi/productlib/orders/OrdersController.kt
@@ -1,27 +1,38 @@
 package com.meshhdawi.productlib.orders
+
+import com.meshhdawi.productlib.context.getUserId
+import com.meshhdawi.productlib.web.security.AuthService
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/orders")
 class OrdersController(
-    private val orderService: OrderService
+    private val orderService: OrderService,
+    private val authService: AuthService
 ) {
 
     @GetMapping("/{id}")
-    fun getOrderById(@PathVariable id: Long): ResponseEntity<OrderEntity> {
-        val order = orderService.getOrderById(id)
-        return ResponseEntity.ok(order)
+    fun getOrderById(@PathVariable id: Long, request: HttpServletRequest): ResponseEntity<OrderEntity> {
+        return authService.validateJWTAuth(request) {
+            val order = orderService.getOrderById(id)
+            if (getUserId() != order.customerId.id) throw IllegalArgumentException("You are not authorized to view orders for this user.")
+            ResponseEntity.ok(order)
+        }
     }
 
     @GetMapping("/user/{customerId}")
-    fun getOrdersByCustomer(@PathVariable customerId: Long): ResponseEntity<List<OrderEntity>> {
-        val orders = orderService.getOrdersByCustomer(customerId)
-        return ResponseEntity.ok(orders)
+    fun getOrdersByCustomer(@PathVariable customerId: Long, request: HttpServletRequest): ResponseEntity<List<OrderEntity>> {
+        return authService.validateJWTAuth(request) {
+            if (getUserId() != customerId) throw IllegalArgumentException("You are not authorized to view orders for this user.")
+            val orders = orderService.getOrdersByCustomer(customerId)
+            ResponseEntity.ok(orders)
+        }
     }
-
     @PostMapping
     fun createOrder(@RequestBody orderRequest: OrderRequest): ResponseEntity<OrderEntity> {
+        if (getUserId() != orderRequest.customerId) throw IllegalArgumentException("You are not authorized to view orders for this user.")
         val createdOrder = orderService.createOrder(orderRequest)
         return ResponseEntity.ok(createdOrder)
     }
--- src/main/kotlin/com/meshhdawi/productlib/web/security/JwtUtil.kt
+++ src/main/kotlin/com/meshhdawi/productlib/web/security/JwtUtil.kt
@@ -2,28 +2,29 @@ package com.meshhdawi.productlib.web.security
 
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
 import org.springframework.stereotype.Component
-import java.util.*
+import java.util.Date
 import javax.crypto.SecretKey
 
 @Component
 class JwtUtil {
-    private val secretKey: SecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+    // TODO make this secret key more secure
+    private final var secret: String = "your-super-secure-256-bit-secret-key-12345"
+    var secretKey: SecretKey = Keys.hmacShaKeyFor(secret.toByteArray(Charsets.UTF_8))
 
     fun generateToken(userId: Long): String {
         val claims: Map<String, Any> = mapOf("userId" to userId)
         return Jwts.builder()
             .setClaims(claims)
             .setIssuedAt(Date(System.currentTimeMillis()))
-            .setExpiration(Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+            .setExpiration(Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 10)) // 10 days
             .signWith(secretKey)
             .compact()
     }
 
     fun extractUserId(token: String): Long {
-        return extractAllClaims(token).get("userId", Integer::class.java)?.toLong() ?: 0L
+        return extractAllClaims(token).get("userId", Integer::class.java)?.toLong() ?: throw IllegalArgumentException("Invalid token")
     }
 
     fun validateToken(token: String): Boolean {
