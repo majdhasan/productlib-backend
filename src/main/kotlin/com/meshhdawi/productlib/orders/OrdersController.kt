8b4cad6552ec0d6d19b7e2aad0df179281272193
Majd Hasan <majd.hasan@bettermile.com>
2025-01-05 06:42:07 +0100

    restrict orders endpoints

--- src/main/kotlin/com/meshhdawi/productlib/orders/OrdersController.kt
+++ src/main/kotlin/com/meshhdawi/productlib/orders/OrdersController.kt
@@ -4,7 +4,12 @@ import com.meshhdawi.productlib.context.getUserId
 import com.meshhdawi.productlib.web.security.AuthService
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/orders")
@@ -23,17 +28,22 @@ class OrdersController(
     }
 
     @GetMapping("/user/{customerId}")
-    fun getOrdersByCustomer(@PathVariable customerId: Long, request: HttpServletRequest): ResponseEntity<List<OrderEntity>> {
+    fun getOrdersByCustomer(
+        @PathVariable customerId: Long,
+        request: HttpServletRequest
+    ): ResponseEntity<List<OrderEntity>> {
         return authService.validateJWTAuth(request) {
             if (getUserId() != customerId) throw IllegalArgumentException("You are not authorized to view orders for this user.")
             val orders = orderService.getOrdersByCustomer(customerId)
             ResponseEntity.ok(orders)
         }
     }
+
     @PostMapping
-    fun createOrder(@RequestBody orderRequest: OrderRequest): ResponseEntity<OrderEntity> {
-        if (getUserId() != orderRequest.customerId) throw IllegalArgumentException("You are not authorized to view orders for this user.")
-        val createdOrder = orderService.createOrder(orderRequest)
-        return ResponseEntity.ok(createdOrder)
-    }
+    fun createOrder(@RequestBody orderRequest: OrderRequest, request: HttpServletRequest): ResponseEntity<OrderEntity> =
+        authService.validateJWTAuth(request) {
+            if (getUserId() != orderRequest.customerId) throw IllegalArgumentException("You are not authorized to create orders for this user.")
+            val createdOrder = orderService.createOrder(orderRequest)
+            ResponseEntity.ok(createdOrder)
+        }
 }
