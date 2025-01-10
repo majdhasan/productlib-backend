b422c1f375012fa490c207c89eacf86c9d619a62
Majd Hasan <majd.hasan@bettermile.com>
2025-01-10 09:45:03 +0100

    aa update and delete product logic

--- src/main/kotlin/com/meshhdawi/productlib/products/ProductController.kt
+++ src/main/kotlin/com/meshhdawi/productlib/products/ProductController.kt
@@ -5,6 +5,7 @@ import com.meshhdawi.productlib.users.UserRole
 import com.meshhdawi.productlib.web.security.AuthService
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -25,10 +26,26 @@ class ProductController(
             ResponseEntity.ok(service.createProduct(product))
         }
 
+    @PostMapping("/{id}")
+    fun updateProduct(@PathVariable id: Long, product: ProductEntity, request: HttpServletRequest): ResponseEntity<ProductEntity> =
+        authService.validateJWTAuth(request) {
+            if (getUserRole() != UserRole.ADMIN) throw IllegalArgumentException("You are not authorized to update a product.")
+            ResponseEntity.ok(service.updateProduct(id, product))
+        }
+
     @GetMapping
     fun getAllProducts(): ResponseEntity<List<ProductEntity>> = ResponseEntity.ok(service.getAllProducts())
 
     @GetMapping("/{id}")
     fun getProductById(@PathVariable id: Long): ResponseEntity<ProductEntity> =
         ResponseEntity.ok(service.getProductsById(id))
+
+    // delete product
+    @DeleteMapping("/{id}")
+    fun deleteProduct(@PathVariable id: Long, request: HttpServletRequest): ResponseEntity<Unit> =
+        authService.validateJWTAuth(request) {
+            if (getUserRole() != UserRole.ADMIN) throw IllegalArgumentException("You are not authorized to delete a product.")
+            service.deleteProduct(id)
+            ResponseEntity.noContent().build()
+        }
 }
--- src/main/kotlin/com/meshhdawi/productlib/products/ProductService.kt
+++ src/main/kotlin/com/meshhdawi/productlib/products/ProductService.kt
@@ -9,8 +9,25 @@ class ProductService(private val repository: ProductRepository) {
 
     fun createProduct(product: ProductEntity): ProductEntity = repository.save(product)
 
+    fun updateProduct(id: Long, product: ProductEntity): ProductEntity {
+        val existingProduct = getProductsById(id)
+        return repository.save(
+            existingProduct.copy(
+                name = product.name,
+                translations = product.translations,
+                image = product.image,
+                description = product.description,
+                cost = product.cost,
+                category = product.category,
+                updatedAt = product.updatedAt
+            )
+        )
+    }
+
     fun getAllProducts(): List<ProductEntity> = repository.findAll()
 
     fun getProductsById(serviceId: Long): ProductEntity =
         repository.findById(serviceId).orElseThrow { IllegalArgumentException("Product with ID $serviceId not found") }
+
+    fun deleteProduct(id: Long) = repository.deleteById(id)
 }
