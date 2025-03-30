ee809d4be00cd941ae513be9c18428990fbe025e
majdhasan <majd.hasan@hotmail.com>
2025-03-30 11:17:54 +0200

    handle various media type requests

--- src/main/kotlin/com/meshhdawi/productlib/web/WebServerConfiguration.kt
+++ src/main/kotlin/com/meshhdawi/productlib/web/WebServerConfiguration.kt
@@ -3,6 +3,8 @@ package com.meshhdawi.productlib.web
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -13,14 +15,23 @@ class WebServerConfiguration {
     private val corsOriginPatterns: String = ""
 
     @Bean
-    fun addCorsConfig(): WebMvcConfigurer {
+    fun webMvcConfigurer(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
+
             override fun addCorsMappings(registry: CorsRegistry) {
                 val allowedOrigins = corsOriginPatterns.split(",").toTypedArray()
                 registry.addMapping("/**")
                     .allowedMethods("*")
                     .allowedOriginPatterns(*allowedOrigins)
                     .allowCredentials(true)
+            }
+
+            override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
+                configurer
+                    .favorPathExtension(false)
+                    .favorParameter(false)
+                    .ignoreAcceptHeader(true)
+                    .defaultContentType(MediaType.APPLICATION_JSON)
             }
         }
     }
